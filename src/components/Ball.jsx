7bfeb46f9da8e23e536abbdd77616f82ad7a30e0
MarcusMahliaire <102937325+MarcusMahliaire@users.noreply.github.com>
2022-07-03 19:54:47 -0700

    Update Ball.jsx

--- src/components/Ball.jsx
+++ src/components/Ball.jsx
@@ -1,17 +1,17 @@
 import React from 'react'
-import { useSphere} from '@react-three/cannon'
-import * as THREE from 'three'
+import { useSphere } from '@react-three/cannon'
+// import * as THREE from 'three'
 
 export default function Ball(props) {
   // const textureLoader = new THREE.TextureLoader()
   // const texture = textureLoader.load('./photos/purple.jpg')
-  
 
-    const [ref] = useSphere(() => ({ mass: 1, position: [0, 5, 0], rotation: [0.4, 1, 0.5], ...props }))
-    return (
-      <mesh receiveShadow castShadow ref={ref}>
-        <sphereGeometry />
-        <meshStandardMaterial color="#ffffff" metalness={.6} roughness={.2} />
-      </mesh>
-    )
-  }
+
+  const [ref] = useSphere(() => ({ mass: 1, position: [0, 5, 0], rotation: [0.4, 1, 0.5], ...props }))
+  return (
+    <mesh receiveShadow castShadow ref={ref}>
+      <sphereGeometry />
+      <meshStandardMaterial color="#ffffff" metalness={.6} roughness={.2} />
+    </mesh>
+  )
+}
